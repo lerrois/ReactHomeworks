2e3c6b4b299c2a71ca38e5c4c4382f2b27e9e6ad
Lerois <lerois.ll@gmail.com>
2025-01-26 15:54:13 +0100

    done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,34 +1,89 @@
-import React, {useState, useEffect}  from 'react';
-import error from "eslint-plugin-react/lib/util/error.js";
+import React, {useState, useEffect} from 'react';
+import './style.sass'
+import Button from "./Button.jsx";
 
 const Todo = () => {
     const [todos, setTodos] = useState([]);
     const API = 'https://679286cdcf994cc6804a5368.mockapi.io/tasks'
+    const tds = [0, 1, 2];
 
     const getTotdos = async () => {
         try {
+            console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
             const request = await fetch(API),
-                  response = await request.json();
+                response = await request.json();
             console.log(response);
             setTodos((prevState) => response);
+        } catch (err) {
+            console.log(err);
         }
-        catch (error) {
-            console.log(error);
-        }
-
-        console.log(todos);
     }
     useEffect(() => {
         getTotdos()
-        }, []);
+    }, []);
 
-    return todos.length ? (
-        <ul>
-            {todos.map((item) => (
-                <li key={item.id}>{item.title}</li>
-            ))}
-        </ul>
-    ): null;
+
+    const handleItemIncrem = async (e, id) => {
+        e.stopPropagation();
+        try {
+            setTodos((prevState) => prevState.map((item, key) => {
+                if (item.id === id) {
+                    const newRecord = {...item};
+                    newRecord.status = newRecord.status + 1;
+                    return newRecord;
+                }
+                else {
+                    return item;
+                }
+            }));
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleItemDecrem = async (e, id) => {
+        try {
+            setTodos((prevState) => prevState.map((item, key) => {
+                if (item.id === id) {
+                    const newRecord = {...item};
+                    newRecord.status = newRecord.status - 1;
+                    return newRecord;
+                }
+                else {
+                    return item;
+                }
+            }));
+
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return <table>
+        <tbody>
+        <tr>
+            <td key={0}>{todos.filter(value => value.status === 0).map((item, i) => <ul key={"ul" + i}>
+                <li key={"li" + i}>{item.title} <Button title="In Progress" handleClick={(e) => handleItemIncrem(e, item.id)} />
+                </li>
+            </ul>)}</td>
+            <td key={1}>{todos.filter(value => value.status === 1).map((item, i) => <ul key={"ul" + i}>
+                <li key={"li" + i}>{item.title}
+                    <Button title="To Do" handleClick={(e) => handleItemDecrem (e, item.id)} />
+                    <Button title="Done" handleClick={(e) => handleItemIncrem(e, item.id)} />
+                </li>
+            </ul>)}</td>
+            <td key={2}>{todos.filter(value => value.status === 2).map((item, i) => <ul key={"ul" + i}>
+                <li key={"li" + i}>{item.title}
+                    <Button title="To Active" handleClick={(e) => handleItemDecrem(e, item.id)} />
+                </li>
+            </ul>)}</td>
+        </tr>
+        </tbody>
+    </table>
+
 };
 
 export default Todo;
+
+
+
