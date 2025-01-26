6df4877b0e077cfd5d3712ebc50a8f681bde0583
Lerois <lerois.ll@gmail.com>
2025-01-26 13:58:56 +0100

    done

--- src/conponents/List.jsx
+++ src/conponents/List.jsx
@@ -13,38 +13,48 @@ export default function List() {
     const [animals, setList] = useState(list);
     const [intervalId, setintervalId] = useState(0);
 
-    if (animals.filter((item) => item.active).length === animals.length) {
-        clearInterval(intervalId);
-    }
-    useEffect( ()=> {
+    useEffect(() => {
         const newIntId = setInterval(() => {
             let isFound = false;
             let result = 0;
-         do {
-             result = Math.floor(Math.random() * animals.length)
-             let obj = animals[result];
-             if (!obj.active) {
-                 isFound = true;
-             }
-         }
-         while (!isFound);
+            do {
+                result = Math.floor(Math.random() * animals.length)
+                let obj = animals[result];
+                if (!obj.active) {
+                    isFound = true;
+                }
+            }
+            while (!isFound);
 
-         setList((prevState) => {
-             let newState = [...prevState];
-             newState[result].active = true;
-             return newState;
-         })
+            setList((prevState) => {
+                let newState = [...prevState];
+                newState[result].active = true;
+                return newState;
+            })
         }, 1000)
         setintervalId(newIntId)
 
-    }, [] );
+    }, []);
+
+    useEffect(() => {
+        const notActivatedList = list.filter((item) => !item.active);
+        if (!notActivatedList.length) clearInterval(intervalId);
+    }, [animals]);
 
     return <table className={'myTable borderclass'}>
-        <tbody>{animals.map((item,key) => item.active ? (
-        <tr className={'borderclass'} key={key}>
-            <td className={'borderclass tdcolorred'}>{item.type}</td>
-            <td className={'borderclass'}>{item.icon}</td></tr>) :
-        (<tr className={'borderclass'} key={key}><td className={'borderclass'}>{item.type}</td>
-            <td className={'borderclass'}>{item.icon}</td></tr>)
-    )} </tbody></table>;
+        {" "}
+        <tbody>
+        {animals.map((item, key) =>
+            item.active ? (
+                <tr className={'borderclass'} key={key}>
+                    <td className={'borderclass tdcolorred'}>{item.type}</td>
+                    <td className={'borderclass'}>{item.icon}</td>
+                </tr>) :
+                (<tr className={'borderclass'} key={key}>
+                    <td className={'borderclass'}>{item.type}</td>
+                    <td className={'borderclass'}>{item.icon}</td>
+                </tr>)
+        )}{" "}
+        </tbody>
+    </table>;
 }
