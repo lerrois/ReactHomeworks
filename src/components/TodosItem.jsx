f12f67b2bd3bc0832056c5e0a6f5b81e52d72354
Lerois <lerois.ll@gmail.com>
2025-01-30 18:14:44 +0100

    almost done

--- src/components/TodosItem.jsx
+++ src/components/TodosItem.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import Button from "./Button.jsx";
 
-function TodosItem({handleItemIncrem}) {
-    return (
-        <li key={"li" + i}>{item.title} <Button title="In Progress"
-                                                handleClick={(e) => handleItemIncrem(e, item.id)}/>
-        </li>
-    );
+
+function TodosItem({i,item,handleItemIncrem,handleItemDecrem,handleItemDelete}) {
+
+    if (handleItemIncrem) {
+        return(
+        <li key={"li" + i}>{item.title}
+            <Button title="In Progress" handleClick={(e) => handleItemIncrem(e, item.id)}/>
+        </li>)
+    }
+    if (handleItemDecrem) {
+        return (
+            <li key={"li" + i}>{item.title}
+                <Button title="To Do" handleClick={(e) => handleItemDecrem(e, item.id)}/>
+                <Button title="Done" handleClick={(e) => handleItemIncrem(e, item.id)}/>
+            </li>
+        );
+    }
+    if (handleItemDelete) {
+        return (
+            <li key={"li" + i}>{item.title}
+                <Button title="To Archive" handleClick={(e) => handleItemDelete(e, item.id)}/>
+            </li>
+        )
+    }
 }
 
 export default TodosItem;
--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import Button from "./Button.jsx";
+import TodosItem from "./TodosItem.jsx";
+
 // import TodosItem from "./TodosItem.jsx";
 
 function TodosList({todos, handleItemIncrem, handleItemDecrem, handleItemDelete}) {
-    console.log("adsjkhdsfgjdgfhjhdfghj")
+    console.log(todos)
     return (
         <tr>
             <td key={0}>{todos.filter(value => value.status === 0).map((item, i) => <ul key={"ul" + i}>
-                <li key={"li" + i}>{item.title} <Button title="In Progress"
-                                                        handleClick={(e) => handleItemIncrem(e, item.id)}/>
-                </li>
+              <TodosItem
+               handleItemIncrem={handleItemIncrem}
+               item={item}
+               i={0}
+              />
             </ul>)}</td>
             <td key={1}>{todos.filter(value => value.status === 1).map((item, i) => <ul key={"ul" + i}>
-                <li key={"li" + i}>{item.title}
-                    <Button title="To Do" handleClick={(e) => handleItemDecrem(e, item.id)}/>
-                    <Button title="Done" handleClick={(e) => handleItemIncrem(e, item.id)}/>
-                </li>
+               <TodosItem
+               handleItemIncrem={handleItemIncrem}
+               handleItemDecrem={handleItemDecrem}
+               item={item}
+               i={1}/>
             </ul>)}</td>
             <td key={2}>{todos.filter(value => value.status === 2).map((item, i) => <ul key={"ul" + i}>
-                <li key={"li" + i}>{item.title}
-                    <Button title="To Archive" handleClick={(e) => handleItemDelete(e, item.id)}/>
-                </li>
+                <TodosItem
+                handleItemDelete={handleItemDelete}
+                item={item}
+                i={2}
+                />
             </ul>)}</td>
         </tr>
         // <td key={0}>{todos.filter(value => value.status === 0).map((item, i) => <ul key={"ul" + i}>
@@ -30,5 +36,22 @@ function TodosList({todos, handleItemIncrem, handleItemDecrem, handleItemDelete}
     )
         ;
 }
+
+// <td key={0}>{todos.filter(value => value.status === 0).map((item, i) => <ul key={"ul" + i}>
+//     <li key={"li" + i}>{item.title} <Button title="In Progress"
+//                                             handleClick={(e) => handleItemIncrem(e, item.id)}/>
+//     </li>
+// </ul>)}</td>
+// <td key={1}>{todos.filter(value => value.status === 1).map((item, i) => <ul key={"ul" + i}>
+//     <li key={"li" + i}>{item.title}
+//         <Button title="To Do" handleClick={(e) => handleItemDecrem(e, item.id)}/>
+//         <Button title="Done" handleClick={(e) => handleItemIncrem(e, item.id)}/>
+//     </li>
+// </ul>)}</td>
+// <td key={2}>{todos.filter(value => value.status === 2).map((item, i) => <ul key={"ul" + i}>
+//     <li key={"li" + i}>{item.title}
+//         <Button title="To Archive" handleClick={(e) => handleItemDelete(e, item.id)}/>
+//     </li>
+// </ul>)}</td>
 
 export default TodosList;
